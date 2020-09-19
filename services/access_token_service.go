8841b066a66162b93b83d8d980b480982fa083f4
Pablo Mendez <mendez.developer@gmail.com>
2020-09-19 18:05:35 -0300

    added business logic to create AT and RT

--- services/access_token_service.go
+++ services/access_token_service.go
@@ -1,44 +1,138 @@
 package services
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	"github.com/google/uuid"
 	"github.com/mendezdev/expenses_oauth-api/domain/access_token"
 	"github.com/mendezdev/expenses_oauth-api/domain/users"
 	"github.com/mendezdev/expenses_oauth-api/http/rest"
+	"github.com/mendezdev/expenses_oauth-api/utils/api_errors"
 )
 
 var (
-	UsersService userServiceInterface = &userService{}
+	AccessTokenService accessTokenServiceInterface = &accessTokenService{}
 )
 
-type userService struct{}
+type accessTokenService struct{}
 
-type userServiceInterface interface {
-	CreateToken(users.UserLoginRequest) (*access_token.AccessToken, error)
+type accessTokenServiceInterface interface {
+	CreateToken(users.UserLoginRequest) (*access_token.AccessToken, error) // change error to api_errors.RestErr
+	TokenValid(string) api_errors.RestErr
+	ExtractTokenMetadata(string) (*access_token.AccessDetails, api_errors.RestErr)
 }
 
-func (s *userService) CreateToken(ur users.UserLoginRequest) (*access_token.AccessToken, error) {
+func (s *accessTokenService) CreateToken(ur users.UserLoginRequest) (*access_token.AccessToken, error) {
 	var err error
-
 	atSecret := "AT_SECRET" //implment this os.Getenv("ACESS_TOKEN_SECRET")
+	rtSecret := "RT_SECRET" //implment this os.Getenv("ACESS_TOKEN_SECRET")
 
-	userReponse, userAPIErr := rest.RestUsersAPI.Login(ur)
+	td := &access_token.TokenDetails{
+		AtExpires:   time.Now().Add(time.Minute * 15).Unix(),
+		AccessUuid:  uuid.New().String(),
+		RtExpires:   time.Now().Add(time.Hour * 24 * 7).Unix(),
+		RefreshUuid: uuid.New().String(),
+	}
+
+	userResponse, userAPIErr := rest.RestUsersAPI.Login(ur)
 	if userAPIErr != nil {
 		return nil, userAPIErr
 	}
 
-	atClaims := jwt.MapClaims{}
-	atClaims["authorized"] = true
-	atClaims["user_id"] = userReponse.ID
-	atClaims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	//Creating acccess token
+	atClaims := buildAccessTokenClaims(userResponse.ID, td)
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, atClaims)
-
-	token, err := at.SignedString([]byte(atSecret))
+	td.AccessToken, err = at.SignedString([]byte(atSecret))
 	if err != nil {
 		return nil, err
 	}
 
-	return &access_token.AccessToken{AccessToken: token}, nil
+	//Creating refresh token
+	rtClaims := buildRefreshTokenClaims(userResponse.ID, td)
+	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, rtClaims)
+	td.RefreshToken, err = rt.SignedString([]byte(rtSecret))
+	if err != nil {
+		return nil, err
+	}
+
+	savedAuthErr := RedisService.CreateAuth(userResponse.ID, td)
+	if savedAuthErr != nil {
+		return nil, savedAuthErr
+	}
+
+	accessToken := &access_token.AccessToken{
+		AccessToken:  td.AccessToken,
+		RefreshToken: td.RefreshToken,
+	}
+	return accessToken, nil
+}
+
+func (s *accessTokenService) verifyToken(at string) (*jwt.Token, api_errors.RestErr) {
+	atSecret := "AT_SECRET" //implment this os.Getenv("ACESS_TOKEN_SECRET")
+	token, err := jwt.Parse(at, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(atSecret), nil
+	})
+
+	if err != nil {
+		return nil, api_errors.NewInternalServerError("error trying to verify token", err)
+	}
+	return token, nil
+}
+
+func (s *accessTokenService) TokenValid(at string) api_errors.RestErr {
+	token, err := s.verifyToken(at)
+	if err != nil {
+		return err
+	}
+
+	if _, ok := token.Claims.(jwt.Claims); !ok && !token.Valid {
+		return api_errors.NewInternalServerError("error trying to validate access token", nil)
+	}
+
+	return nil
+}
+
+func (s *accessTokenService) ExtractTokenMetadata(at string) (*access_token.AccessDetails, api_errors.RestErr) {
+	token, err := s.verifyToken(at)
+	if err != nil {
+		return nil, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		accessUuid, ok := claims["access_uuid"].(string)
+		if !ok {
+			return nil, api_errors.NewInternalServerError("error trying to parse access_uuid", nil)
+		}
+		userID := claims["user_id"].(string)
+
+		return &access_token.AccessDetails{
+			AccessUuid: accessUuid,
+			UserID:     userID,
+		}, nil
+	}
+
+	return nil, api_errors.NewInternalServerError("error trying to extract token metadata", nil)
+}
+
+func buildAccessTokenClaims(userID string, td *access_token.TokenDetails) jwt.MapClaims {
+	atClaims := jwt.MapClaims{}
+	atClaims["authorized"] = true
+	atClaims["access_uuid"] = td.AccessUuid
+	atClaims["user_id"] = userID
+	atClaims["exp"] = td.AtExpires
+	return atClaims
+}
+
+func buildRefreshTokenClaims(userID string, td *access_token.TokenDetails) jwt.MapClaims {
+	rtClaims := jwt.MapClaims{}
+	rtClaims["refresh_uuid"] = td.RefreshUuid
+	rtClaims["user_id"] = userID
+	rtClaims["exp"] = td.RtExpires
+	return rtClaims
 }
