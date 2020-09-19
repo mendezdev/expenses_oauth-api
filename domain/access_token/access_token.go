1974234de7ef46addfd10c12cba9a15354d04162
Pablo Mendez <mendez.developer@gmail.com>
2020-09-19 17:53:57 -0300

    update and added new domains

--- domain/access_token/access_token.go
+++ domain/access_token/access_token.go
@@ -1,5 +1,23 @@
 package access_token
 
+// AccessToken is the response to the public client
 type AccessToken struct {
-	AccessToken string `json:"access_token"`
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+}
+
+// TokenDetails contains the token info
+type TokenDetails struct {
+	AccessToken  string
+	RefreshToken string
+	AccessUuid   string
+	RefreshUuid  string
+	AtExpires    int64
+	RtExpires    int64
+}
+
+// AccessDetails is the dto for db data
+type AccessDetails struct {
+	AccessUuid string `json:"access_uuid"`
+	UserID     string `json:"user_id"`
 }
